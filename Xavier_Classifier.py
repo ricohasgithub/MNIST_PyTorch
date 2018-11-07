503cab586ed696d21fb65136f47fcb7b0d7441b2
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2018-11-07 11:53:49 -0500

    Created Xavier_Classifier.py
     - Added import statements

---
+++ Xavier_Classifier.py
@@ -0,0 +1,9 @@
+import torch 
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+import torch.nn.functional as F
+import matplotlib.pylab as plt
+import numpy as np
+torch.manual_seed(0)
+
